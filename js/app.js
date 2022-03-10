441af070ee6043cbc7ec87f30fd6c2d41178ce80
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-10 22:19:38 +0600

    [Bug Fixed] : Display user name and user comments

--- js/app.js
+++ js/app.js
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments?.map(comment => `${comment.user}`)}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments?.map(comment => `${comment.text}`)}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
